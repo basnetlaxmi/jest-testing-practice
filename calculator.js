0443ddb765247d41d3ac6a350db3236fd7dfd0c9
basnetlaxmi <ddsanzel@gmail.com>
2021-10-13 16:41:01 +0545

    Add calculator class and test function for calculator

---
+++ calculator.js
@@ -0,0 +1,23 @@
+class Calculator {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    add() {
+        return this.a + this.b;
+    }
+
+    sub() {
+        return this.a - this.b;
+    }
+
+    multiply() {
+        return this.a * this.b;
+    }
+
+    division() {
+        return this.a / this.b;
+    }
+}
+
---
+++ calculator.test.js
@@ -0,0 +1,13 @@
+const Calculator = require("./calculator.js");
+
+describe("Additions", () => {
+  const calculator = new Calculator(a, b);
+  test("add positive numbers", () => {
+    expect(Calculator.add(12, 20)).toBe(32);
+    expect(Calculator.add(3990, 10)).toBe(4000);
+  });
+
+//   test("add negative numbers", () => {
+//     expect(Calculator.add).toBe(-10);
+//   });
+});
