8a061ccf1ff4b956a3d6955cc576d484901493c0
basnetlaxmi <ddsanzel@gmail.com>
2021-10-13 15:59:53 +0545

    Add function and testing on Minimum length and Maximum length

--- strLength.test.js
+++ strLength.test.js
@@ -5,3 +5,11 @@ test('String Length Value', () => {
     expect(stringLength('Laxmi')).toBe(5);
     expect(stringLength('Basnet')).toBe(6);
 });
+
+test('Min Length Error', () => {
+    expect(() => {stringLength('')}).toThrow(Error);
+});
+
+test('Max Length Error', () => {
+    expect(() => stringLength('Disadvantage')).toThrow(/MAX_LENGTH_ERROR/);
+});
--- stringLength.js
+++ stringLength.js
@@ -1,4 +1,6 @@
 function stringLength(str) {
+    if(str.length < 1 ) throw new Error('MIN_LENGTH_ERROR: The length of the string should be greater than 1.');
+    if(str.length > 10 ) throw new Error('MAX_LENGTH_ERROR: The length of the string should not greater than 10');
     return(str.length);
 }
 
